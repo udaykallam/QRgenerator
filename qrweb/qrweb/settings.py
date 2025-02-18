68c20648736444bb386185afc3184643db0b8b22
Uday <udayreddykallam@gmail.com>
2025-02-18 10:10:15 +0530

    Updated

--- qrweb/qrweb/settings.py
+++ qrweb/qrweb/settings.py
@@ -92,4 +92,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- qrweb/user/views.py
+++ qrweb/user/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from qrcode import *
-
-data = None
+import qrcode
+import os
+from django.conf import settings
 
 def index(request):
-    global data
+    data = None
+    img_path = None  # To store the dynamically generated QR code path
+
     if request.method == 'POST':
         data = request.POST['data']
-        img = make(data)
-        img.save('static/qrweb/images/test.png')
-    else:
-        pass
-    return render(request, 'user/index.html', {'data': data})
+        img = qrcode.make(data)
+
+        # Define the path to save the QR code image inside STATICFILES_DIRS
+        img_dir = os.path.join(settings.MEDIA_ROOT, 'qrweb/images')
+        os.makedirs(img_dir, exist_ok=True)  # Ensure the directory exists
+
+        img_path = os.path.join(img_dir, 'test.png')
+        img.save(img_path)
+
+        # Convert file path to a relative URL
+        img_path = f"/media/qrweb/images/test.png"
+
+    return render(request, 'user/index.html', {'data': data, 'img_path': img_path})
