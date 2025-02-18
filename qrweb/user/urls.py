384898fde702352b3f3c61dbf701bc46cfb1966b
Uday <udayreddykallam@gmail.com>
2025-02-18 10:12:38 +0530

    Final Commit

--- qrweb/user/urls.py
+++ qrweb/user/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
 ]
 
-# Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- qrweb/user/views.py
+++ qrweb/user/views.py
@@ -5,20 +5,13 @@ from django.conf import settings
 
 def index(request):
     data = None
-    img_path = None  # To store the dynamically generated QR code path
-
+    img_path = None
     if request.method == 'POST':
         data = request.POST['data']
         img = qrcode.make(data)
-
-        # Define the path to save the QR code image inside STATICFILES_DIRS
         img_dir = os.path.join(settings.MEDIA_ROOT, 'qrweb/images')
-        os.makedirs(img_dir, exist_ok=True)  # Ensure the directory exists
-
+        os.makedirs(img_dir, exist_ok=True)
         img_path = os.path.join(img_dir, 'test.png')
         img.save(img_path)
-
-        # Convert file path to a relative URL
         img_path = f"/media/qrweb/images/test.png"
-
     return render(request, 'user/index.html', {'data': data, 'img_path': img_path})
